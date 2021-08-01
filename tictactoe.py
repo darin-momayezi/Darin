9234a985380376ded1ebc60dda06cb5b47131664
darin-momayezi <d.momayezi@gmail.com>
2021-08-01 14:33:45 -0400

    tic tac toe game

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,7 @@
 # tic tac toe
 
+from __future__ import print_function
+
 board = [
     ["-", "-", "-"],
     ["-", "-", "-"],
@@ -12,30 +14,51 @@ def print_board(board):
             print(slot + " ", end="")
         print()
 
-print_board(board)
-
 def quit(user_input):
     if user_input == "q": return True
     else: return False
 
 def check_input(user_input):
-    # check if it is a number
-    isnum(user_input)
+    if 0 < int(user_input) < 10: return True
+    else: return False
 
-    # check if it is 1 through 9
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "-":
+        print("This position is already taken.")
+        return True
+    else: return False
 
-def isnum(user_input):
-    if not user_input.isnumeric():
-        print("This is not a valid number.")
-        return False
-    else: return True
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = int(user_input % 3)
+    if col == 0:
+        col = int(user_input)
+    else: return (row, col)
+    return (row, col)
+
+def add_to_board(coords, board):
+    row = coords[0]
+    col = coords[1]
+    board[row][col] = "x"
 
 while True:
+    print_board(board)
     user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit. ")
     if quit(user_input): 
         print("Thanks for playing!")
         break
 
     if not check_input(user_input):
-        print("Please enter an number between 1 and 9.")
+        print("Please enter a number between 1 and 9.")
         continue
+
+    # user input starts from one but indexing starts from 0
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    if istaken(coords, board):
+        print("Please try again.")
+        continue
+
+    add_to_board(coords, board)
