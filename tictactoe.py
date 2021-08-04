c710ad3ce83d078fd539e911215af857e5d8d20e
darin-momayezi <d.momayezi@gmail.com>
2021-08-04 15:45:12 -0400

    tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,8 @@ board = [
     ["-", "-", "-"]
 ]
 
+user = True # when user is True it refers to "x"
+
 def print_board(board):
     for row in board:
         for slot in row:
@@ -38,15 +40,24 @@ def coordinates(user_input):
     else: return (row, col)
     return (row, col)
 
-def add_to_board_player_one(coords, board):
+def add_to_board(coords, board):
     row = coords[0]
     col = coords[1]
-    board[row][col] = "x"
+    board[row][col] = active_user(user)
 
-def add_to_board_player_two(coords, board):
-    row = coords[0]
-    col = coords[1]
-    board[row][col] = "o"
+def active_user(user):
+    if user: return "x"
+    else: return "o"
+
+def iswin(coords, board):
+    for s in board[row].items():
+        if s == "x":
+            print("Player 1 won!")
+            break
+        elif s == "o":
+            print("Player 2 won!")
+            break
+        else: continue
 
 while True:
     print_board(board)
@@ -68,24 +79,6 @@ while True:
         print("Please try again.")
         continue
 
-    add_to_board_player_one(coords, board)
+    add_to_board(coords, board)
 
-    print_board(board)
-
-    # Player 2
-    user_input = raw_input("Player 2, please enter a position 1 through 9 or enter \"q\" to quit. ")
-    if quit(user_input): 
-        print("Thanks for playing!")
-        break
-
-    if not check_input(user_input):
-        print("Please enter a number between 1 and 9.")
-        continue
-
-    user_input = int(user_input) - 1
-    coords = coordinates(user_input)
-    if istaken(coords, board):
-        print("Please try again.")
-        continue
-
-    add_to_board_player_two(coords, board)
+    user = not user # toggle between users
