4f8e3324a04c7082ad13c8dc60d9871b119cc66b
darin-momayezi <d.momayezi@gmail.com>
2021-08-03 20:43:44 -0400

    tic tac toe game

--- tictactoe.py
+++ tictactoe.py
@@ -3,8 +3,8 @@
 from __future__ import print_function
 
 board = [
-    ["-", "-", "-"],
-    ["-", "-", "-"],
+    ["-", "-", "-"], 
+    ["-", "-", "-"], 
     ["-", "-", "-"]
 ]
 
@@ -33,19 +33,26 @@ def istaken(coords, board):
 def coordinates(user_input):
     row = int(user_input / 3)
     col = int(user_input % 3)
-    if col == 0:
+    if user_input <= 2:
         col = int(user_input)
     else: return (row, col)
     return (row, col)
 
-def add_to_board(coords, board):
+def add_to_board_player_one(coords, board):
     row = coords[0]
     col = coords[1]
     board[row][col] = "x"
 
+def add_to_board_player_two(coords, board):
+    row = coords[0]
+    col = coords[1]
+    board[row][col] = "o"
+
 while True:
     print_board(board)
-    user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit. ")
+
+    # Player 1
+    user_input = raw_input("Player 1, please enter a position 1 through 9 or enter \"q\" to quit. ")
     if quit(user_input): 
         print("Thanks for playing!")
         break
@@ -61,4 +68,24 @@ while True:
         print("Please try again.")
         continue
 
-    add_to_board(coords, board)
+    add_to_board_player_one(coords, board)
+
+    print_board(board)
+
+    # Player 2
+    user_input = raw_input("Player 2, please enter a position 1 through 9 or enter \"q\" to quit. ")
+    if quit(user_input): 
+        print("Thanks for playing!")
+        break
+
+    if not check_input(user_input):
+        print("Please enter a number between 1 and 9.")
+        continue
+
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    if istaken(coords, board):
+        print("Please try again.")
+        continue
+
+    add_to_board_player_two(coords, board)
